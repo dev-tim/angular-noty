67b477ba38c93f7713fce92b9ec7a249c946a694
azadorozhniy <artem.zadorozhniy@gmail.com>
2014-08-18 14:23:30 +0200

    Added concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
-		connect = require('gulp-connect');
+		connect = require('gulp-connect'),
+		concat = require('gulp-concat');
+
+
+gulp.task('scripts', function() {
+	gulp.src(['bower_components/noty/js/noty/packaged/jquery.noty.packaged.min.js','./angular-noty.js'])
+			.pipe(concat('angular-noty.dist.js'))
+			.pipe(gulp.dest('dist'))
+});
 
 gulp.task('connect', function() {
 	connect.server();
 });
 
-gulp.task('default', ['connect']);
+gulp.task('default', ['scripts','connect']);
